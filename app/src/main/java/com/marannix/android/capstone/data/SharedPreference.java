34a14c5f445b077808080a494d901d5030dba81a
Joshua Onabanjo <joshua@memrise.com>
2018-07-31 19:51:38 +0100

    Created a shared preference which will be useful to pass movies to the widget

---
+++ app/src/main/java/com/marannix/android/capstone/data/SharedPreference.java
@@ -0,0 +1,23 @@
+package com.marannix.android.capstone.data;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+import com.google.gson.Gson;
+import com.marannix.android.capstone.data.model.Movie;
+
+public class SharedPreference {
+
+  private final static String MOVIE_KEY = "MOVIE";
+
+  public static Movie getSharedPreferences(Context context) {
+    Gson gson = new Gson();
+    String json = PreferenceManager.getDefaultSharedPreferences(context).getString(MOVIE_KEY, "");
+    return gson.fromJson(json, Movie.class);
+  }
+
+  public static void setSharedPreferences(Context context, Movie movie) {
+    Gson gson = new Gson();
+    String json = gson.toJson(movie);
+    PreferenceManager.getDefaultSharedPreferences(context).edit().putString(MOVIE_KEY, json).apply();
+  }
+}
