568bab2efc38a3b546a60e4a679df6e30b667c63
Joshua Onabanjo <joshua@memrise.com>
2018-08-01 19:31:07 +0100

    Update the widget view

--- app/src/main/java/com/marannix/android/capstone/widget/FavouriteMovieWidgetProvider.java
+++ app/src/main/java/com/marannix/android/capstone/widget/FavouriteMovieWidgetProvider.java
@@ -1,11 +1,15 @@
 package com.marannix.android.capstone.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 import com.marannix.android.capstone.R;
+import com.marannix.android.capstone.activity.HomeActivity;
+import com.marannix.android.capstone.data.SharedPreference;
 
 /**
  * Implementation of App Widget functionality.
@@ -14,12 +18,28 @@ public class FavouriteMovieWidgetProvider extends AppWidgetProvider {
 
   static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
-    CharSequence widgetText = context.getString(R.string.appwidget_text);
-    // Construct the RemoteViews object
     RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.favourite_movie_widget);
-    views.setTextViewText(R.id.appwidget_text, widgetText);
 
-    // Instruct the widget manager to update the widget
+    if (SharedPreference.getSharedPreferences(context) != null) {
+
+      views.setTextViewText(R.id.favourite_item_widget_title,
+          SharedPreference.getSharedPreferences(context).getTitle());
+    } else {
+      views.setTextViewText(R.id.favourite_item_widget_title,
+          "You have no Favourite Movies");
+    }
+
+    Intent intent = new Intent(context, FavouriteMovieWidgetService.class);
+
+    //TODO: I need to create a Favourite word activity.
+    Intent activity = new Intent(context, HomeActivity.class);
+
+    views.setRemoteAdapter(R.id.widgetListView, intent);
+
+    PendingIntent pendingIntent =
+        PendingIntent.getActivity(context, 0, activity, PendingIntent.FLAG_UPDATE_CURRENT);
+    views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+
     appWidgetManager.updateAppWidget(appWidgetId, views);
   }
 
@@ -32,6 +52,17 @@ public class FavouriteMovieWidgetProvider extends AppWidgetProvider {
 
   @Override public void onReceive(Context context, Intent intent) {
     super.onReceive(context, intent);
+
+    String action = intent.getAction();
+    if (action.equals("com.example.android.capstone.action.update.widget")) {
+      AppWidgetManager appWidgetManager =
+          AppWidgetManager.getInstance(context.getApplicationContext());
+      ComponentName widget =
+          new ComponentName(context.getApplicationContext(), FavouriteMovieWidgetProvider.class.getName());
+      int[] appWidgetIds = appWidgetManager.getAppWidgetIds(widget);
+      appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.favourite_item_widget_title);
+      onUpdate(context, appWidgetManager, appWidgetIds);
+    }
   }
 
   @Override public void onEnabled(Context context) {
